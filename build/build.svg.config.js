ad39c5870b0b8afb5c8b24d4c99dfdf51d766760
DESKTOP-4SPP55M\Travis <godxiaoji@vip.qq.com>
2021-12-22 00:17:34 +0800

    feat(svg): sVG组件优化

---
+++ build/build.svg.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  dir: './assets/icons',
+  output: './src/Icon/icons.js',
+  prefix: 'fx'
+}
--- src/Icon/SvgIcon.vue
+++ src/Icon/SvgIcon.vue
@@ -1,18 +1,29 @@
 <template>
   <svg>
-    <use :xlink:href="iconName"></use>
+    <use :xlink:href="useName"></use>
   </svg>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import './icons'
+import { computed, defineComponent } from 'vue'
+import icons from './icons'
 
 export default defineComponent({
   name: 'fx-svg-icon',
   props: {
     iconName: {
       type: String
+    }
+  },
+  setup(props) {
+    const useName = computed(() => {
+      return `#${(icons as any)['fx-' + props.iconName] ? 'fx-' : ''}${
+        props.iconName
+      }`
+    })
+
+    return {
+      useName
     }
   }
 })
